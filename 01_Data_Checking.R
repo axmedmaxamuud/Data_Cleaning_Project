f2b2d8764c18971aa1f85bd6307c59b749ca9bed
axmedmaxamuud <52702274+axmedmaxamuud@users.noreply.github.com>
2022-03-24 14:33:46 +0300

    test

--- 01_Data_Checking.R
+++ 01_Data_Checking.R
@@ -8,6 +8,7 @@ library(lubridate)
 library(tidyr)
 library(readxl)
 library(openxlsx)
+library(digest)
 source("functions/cleaning_functions.R")
 
 # initalize common variables
@@ -17,6 +18,7 @@ survey_end_date <- "5/1/2022"
 # load raw data
 data <- read.csv("input/testdf.csv", stringsAsFactors = FALSE)
 checklist <- readxl::read_excel("input/check_list.xlsx")
+ki_info <- read.csv("input/hash_itemset.csv", stringsAsFactors = FALSE)
 logbook <- as.data.frame(logbook())
 
 ## Standard Checks
@@ -40,9 +42,19 @@ check_age <- data %>%
   filter(ki_age > 110) %>% log_sheet(question.name = "ki_age", issue = "please confirm the reported ki age", action = "flag")
 logbook <- rbind(logbook, check_age)
 
-# check 5 - income check
+# check 5 - check isuses from the checklist
+run_all_checks <- run_checks_from_dataframe(df = data,
+                                            conditions_df = checklist,
+                                            condition.column = "check",
+                                            test.name.column = "name",
+                                            meta_to_keep = c("uuid", "region"))
 
+# check survey tracker
+tracker <- survey_tracker(data, region = "region", district = "district")
 
+# check enumerator productivity
+enumerator_productivity <- enum_productivity(data, enum_name = "enum_name")
 
-## Export logbood
+## Export log_bood
 write.xlsx(logbook, paste0("output/logbook_", today,".xlsx"))
+
